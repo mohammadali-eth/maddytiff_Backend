e331e94786cc82bbf0f57f28cdc2ba4fed7477b5
MohammadAli <109208273+MohammadAli440@users.noreply.github.com>
2026-01-07 18:50:26 +0530

    deploy server

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || '*',
+    credentials: true
+}));
 app.use(express.json());
 
 // Connect Database
